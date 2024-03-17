d8ea8b0b25bc44567f8c3bfc0280807d5d3b8451
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-03-17 21:33:03 +0300

    splash screen tasarım

---
+++ app/src/main/java/com/yasincakir/splash_screen_renta_car/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.yasincakir.splash_screen_renta_car
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var topAnim:Animation
+    lateinit var bottomAnim:Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window?.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        topAnim=AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_animation)
+
+
+        splashResmi.setAnimation(topAnim)
+        textView.setAnimation(bottomAnim)
+        textView2.setAnimation(bottomAnim)
+
+
+        val handler=Handler()
+
+        handler.postDelayed({
+
+            var intent= Intent(this,loginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+
+    }
+}
---
+++ app/src/main/java/com/yasincakir/splash_screen_renta_car/loginActivity.kt
@@ -0,0 +1,11 @@
+package com.yasincakir.splash_screen_renta_car
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
