a0837e6e0f398fdab0e237bfb15c752863f07a0c
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-03-18 13:01:11 +0300

    login tasarimi

--- app/src/main/java/com/yasincakir/splash_screen_renta_car/loginActivity.kt
+++ app/src/main/java/com/yasincakir/splash_screen_renta_car/loginActivity.kt
@@ -1,11 +1,25 @@
 package com.yasincakir.splash_screen_renta_car
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+
+    fun newUser(view : View){
+
+        var intent= Intent(this,MainActivity2::class.java)
+
+        startActivity(intent)
+        finish()
+
+    }
+
+
 }
