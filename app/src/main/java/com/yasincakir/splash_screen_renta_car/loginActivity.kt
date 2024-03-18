47df2d441cc7466ff4bf836a2d7f1d3e23c21021
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-03-18 14:12:15 +0300

    login devam

--- app/src/main/java/com/yasincakir/splash_screen_renta_car/loginActivity.kt
+++ app/src/main/java/com/yasincakir/splash_screen_renta_car/loginActivity.kt
@@ -4,11 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_login.*
 
 class loginActivity : AppCompatActivity() {
+
+    lateinit var rightAnim : Animation
+    lateinit var leftAnim : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+    rightAnim=AnimationUtils.loadAnimation(this,R.anim.right_animation)
+    leftAnim=AnimationUtils.loadAnimation(this,R.anim.left_animation)
+
+
+    loga_image.setAnimation(rightAnim)
+    logo_name.setAnimation(leftAnim)
+
+
+
     }
 
 
