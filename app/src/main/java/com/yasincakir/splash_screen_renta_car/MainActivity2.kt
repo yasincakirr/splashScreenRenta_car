c5dc32e330cc81eacf0720595e5f617c739c7583
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-03-19 13:36:26 +0300

    register sayfasi

--- app/src/main/java/com/yasincakir/splash_screen_renta_car/MainActivity2.kt
+++ app/src/main/java/com/yasincakir/splash_screen_renta_car/MainActivity2.kt
@@ -1,11 +1,45 @@
 package com.yasincakir.splash_screen_renta_car
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var rightanim : Animation
+    lateinit var leftanim : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
+
+        rightanim=AnimationUtils.loadAnimation(this,R.anim.right_animation)
+        leftanim=AnimationUtils.loadAnimation(this,R.anim.left_animation)
+
+
+
+        imageView.setAnimation(rightanim)
+        txtwelcome.setAnimation(leftanim)
+
+
     }
+
+
+
+    fun turnBack(view : View) {
+
+        var intent=Intent(this,loginActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+
+
+
 }
