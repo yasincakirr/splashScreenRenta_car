8539eefa261e79ccc45b20d5a5d731a455751479
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-03-24 13:32:40 +0300

    register kontrol

--- app/src/main/java/com/yasincakir/splash_screen_renta_car/MainActivity2.kt
+++ app/src/main/java/com/yasincakir/splash_screen_renta_car/MainActivity2.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main2.view.*
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -28,7 +30,84 @@ class MainActivity2 : AppCompatActivity() {
         txtwelcome.setAnimation(leftanim)
 
 
+
+
+
+
+
+
+
     }
+
+
+
+
+    fun register(view: View){
+
+
+
+
+        if(til_fullname.fullname.text.toString() == ""){
+
+            til_fullname.error="Metin Boş Döndürülemez"
+            til_fullname.isErrorEnabled=true
+        }
+        else{
+            til_fullname.isErrorEnabled=false
+        }
+
+
+
+        if(til_username.usernamee.text.toString() == ""){
+            til_username.error="Metin Boş Döndürülemez"
+            til_username.isErrorEnabled=true
+        }
+        else{
+            til_username.isErrorEnabled=false
+        }
+
+
+
+
+
+        if(til_email.emaill.text.toString() == ""){
+
+            til_email.error="Metin Boş Döndürülemez"
+            til_email.isErrorEnabled=true
+        }
+        else{
+            til_email.isErrorEnabled=false
+        }
+
+
+
+        if(til_phoneNo.phoneNo.text.toString() == ""){
+            til_phoneNo.error="Metin Boş Döndürülemez"
+            til_phoneNo.isErrorEnabled=true
+        }
+        else{
+            til_phoneNo.isErrorEnabled=false
+        }
+
+
+
+
+
+
+        if(til_sifre.sifre.text.toString() == ""){
+            til_sifre.error="Metin Boş Döndürülemez"
+            til_sifre.isErrorEnabled=true
+        }
+        else{
+            til_sifre.isErrorEnabled=false
+        }
+
+
+
+    }
+
+
+
 
 
 
